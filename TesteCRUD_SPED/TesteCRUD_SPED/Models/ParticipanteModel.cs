045af2bb4a795a0689b3a8be302ef6f108dd8b1e
Diorgines Lacerda <diorgineslacerda@gmail.com>
2018-03-27 18:43:02 -0300

    Criada a ParticipanteModel

---
+++ TesteCRUD_SPED/TesteCRUD_SPED/Models/ParticipanteModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using TesteCRUD_SPED.Entities;
+
+namespace TesteCRUD_SPED.Models
+{
+    public class ParticipanteModel
+    {
+        
+        public int Cod_Part { get; set; }
+
+        [Required]
+        public string Nome { get; set; }
+
+        [Required]
+        public Pais Pais { get; set; }
+
+
+        public string CNPJ { get; set; }
+
+        public string CPF { get; set; }
+
+        public string IE { get; set; }
+
+        public Municipio Municipio { get; set; }
+
+        public string SUFRAMA { get; set; }
+
+        [Required]
+        public string Endereço { get; set; }
+
+        public int Numero { get; set; }
+
+        public string Complemento { get; set; }
+
+        public string Bairo { get; set; }
+
+        public bool IsentoIE { get; set; }
+
+    }
+}
