c0bc1c2f1912228c031e5729ee44cadac23138f0
Diorgines Lacerda <diorgineslacerda@gmail.com>
2018-03-27 18:33:18 -0300

    Criação de participante Controller

---
+++ TesteCRUD_SPED/TesteCRUD_SPED/Controllers/ParticipanteController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TesteCRUD_SPED.Controllers
+{
+    public class ParticipanteController : Controller
+    {
+        
+        public ActionResult Form()
+        {
+            return View();
+        }
+
+        public ActionResult Add()
+        {
+            return View();
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
