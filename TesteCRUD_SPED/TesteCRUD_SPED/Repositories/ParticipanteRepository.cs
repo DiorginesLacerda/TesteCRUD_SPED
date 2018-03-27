b10ee98afc3f87c39cbddda9a6aa19da430654a2
Diorgines Lacerda <diorgineslacerda@gmail.com>
2018-03-27 18:11:33 -0300

    Criada a classe ParticipanteRepositoy

---
+++ TesteCRUD_SPED/TesteCRUD_SPED/Repositories/ParticipanteRepository.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using TesteCRUD_SPED.Entities;
+
+namespace TesteCRUD_SPED.Repositories
+{
+    public class ParticipanteRepository
+    {
+        private CrudSpedContext context;
+
+        public ParticipanteRepository(CrudSpedContext context)
+        {
+            this.context = context;
+        }
+
+        public void Add(Participante participante)
+        {
+            context.Participantes.Add(participante);
+            context.SaveChanges();
+        }
+
+        public IList<Participante> GetAll()
+        {
+            return context.Participantes.ToList();
+        }
+
+        public Participante FindById(string cod)
+        {
+            return context.Participantes.Where(p => p.COD_PART.Equals(cod)).First();
+        }
+
+        public void Update(string cod)
+        {
+
+        }
+
+    }
+}
