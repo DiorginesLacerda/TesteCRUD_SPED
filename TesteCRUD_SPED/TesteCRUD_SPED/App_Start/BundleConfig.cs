26426cc4dcfa020fdd4ef0f987c518f6f6670582
Diorgines Lacerda <diorgineslacerda@gmail.com>
2018-03-29 16:13:56 -0300

    Inicio dos Testes

--- TesteCRUD_SPED/TesteCRUD_SPED/App_Start/BundleConfig.cs
+++ TesteCRUD_SPED/TesteCRUD_SPED/App_Start/BundleConfig.cs
@@ -9,7 +9,8 @@ namespace TesteCRUD_SPED
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                        "~/Scripts/jquery-{version}.js",
+                        "~/Scripts/jqueryform_participante.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
---
+++ TesteCRUD_SPED/TesteCRUD_SPED/Scripts/jqueryform_participante.js
@@ -0,0 +1,51 @@
+﻿
+var pais = $('#pais')
+
+var pfpj = $("#pfpj")
+var pjpf = $("#pjpf")
+var cpf = $("#cpf")
+var cnpj = $("#cnpj")
+var estado = $("#estado")
+var ie = $("#ie")
+var suframa = $("#suframa")
+var municipio = $("#municipio")
+var bairro = $("#bairro")
+var numero = $("#numero")
+var complemento = $("#complemento")
+
+
+
+$(document).ready(function () {
+    pais.val('1058')
+    pjpf
+
+    pais.on("change", function () {
+        if (pais.val() != '1058') {
+            pfpj.hide()
+            cpf.hide()
+            cnpj.hide()
+            estado.hide()
+            ie.hide()
+            suframa.hide()
+            municipio.hide()
+            bairro.hide()
+            numero.hide()
+            complemento.hide()
+
+        } else {
+            pfpj.show()
+            cpf.show()
+            cnpj.show()
+            estado.show()
+            ie.show()
+            suframa.show()
+            municipio.show()
+            bairro.show()
+            numero.show()
+            complemento.show()
+        }
+            
+    })
+});
+
+
